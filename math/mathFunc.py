1ca6dc4e2f0eb9012beac41062ab44c089b96813
ChucklesDroid <mj.akarsh@gmail.com>
2022-02-03 04:46:28 +0530

    Making use of math libraries in Python

---
+++ math/mathFunc.py
@@ -0,0 +1,27 @@
+import math #for math functions(includes math library ,here in this case math.py)
+
+#  Functions used that are present in math library are :-
+#  (1)ceil
+#  (2)floor
+#  (3)sqrt
+
+#  Remaining Functions that can be called directly :-
+#  (1)pow
+#  (2)abs
+#  (3)round
+#  (4)max
+#  (4)min
+
+pi = 3.14
+#  pi = -3.14
+#  print(round(pi))         #Rounding to nearest 0
+#  print(math.ceil(pi))     #Smallest integer value that is bigger than or equal to the given value 
+#  print(math.floor(pi))    #Greatest integer value that is less than or equal to the given value
+#  print(abs(pi))           #Gives absolute value i.e magnitude(value changed temporarily to negative )
+#  print(pow(pi,2))         #Calculates the power (For example here calculates square of pi)
+#  print(math.sqrt(441))    #Calculates the square root of the value
+x , y , z = 1,2,4
+print(max(x,y,z,pi))        #Calculates the maximum among the passed values
+print(min(z,pi))            #Calculates the minimum among the passed values
+
+   
