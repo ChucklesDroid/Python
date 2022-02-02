5b7d16536bbf2862683c34e56e59c433b0f2c722
ChucklesDroid <mj.akarsh@gmail.com>
2022-02-02 06:20:24 +0530

    Multi Line assignments

---
+++ dataTypes/multiAssign.py
@@ -0,0 +1,26 @@
+#  we can assign multiple variables in python either on seperate lines or on single line. For Example:-
+
+#  name = "aakarsh"
+#  age = 21
+#  height = 185.5
+#  relationship = False
+
+#  or we can declare and initialize them in one line
+name, age, height, relationship = "aakarsh", 21, 185.5, False
+print(name)
+print(age)
+print(height)
+print(relationship)
+
+#  we can look at one more example of multiline assignment
+#  Example :-
+#  akku = 21
+#  aakash = 21
+#  happy = 21
+
+#  Or we can make use of multi line assignment
+akku = aakash = happy = 21
+
+print(akku)
+print(aakash)
+print(happy)
