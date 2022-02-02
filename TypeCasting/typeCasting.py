5b6e10d82b32867b174d4b9f7b5341767ed61964
ChucklesDroid <mj.akarsh@gmail.com>
2022-02-02 07:33:13 +0530

    Type casting in python

---
+++ TypeCasting/typeCasting.py
@@ -0,0 +1,38 @@
+#  Type casting in python 
+
+x = 1   #integer value
+y = 2.0 #float value
+z = "3" #string value
+
+#  To type cast int to float
+x = float(x)
+print(type(x))
+
+#  To type cast int to string
+x = int(x)
+x = str(x)
+print(type(x))
+
+#  To type cast float to integer 
+y = int(y)
+print(type(y))
+
+#  To type cast float to string
+y = str(y)
+print(type(y))
+
+#  To type cast string to float
+z = float(z)
+print(type(z)) 
+print(z)
+
+#  To type cast string to int
+z = "3"
+z = int(z)
+print( type(z) )
+print(z)
+
+#  print(type(x))
+#  print(type(y))
+#  print(type(z))
+#  print(z*3)  #it prints string z 3 times
