76c01ae122d968bb89c1101c9a56e431a216bc1d
ChucklesDroid <mj.akarsh@gmail.com>
2022-02-03 19:56:44 +0530

    Added nested loops in python

---
+++ loops/nestedlops.py
@@ -0,0 +1,11 @@
+#  print statement has a parameter 'end' which by default has value '\n'
+#  but we can also explicity provide it value according to our needs
+
+#  Example program to print pattern
+
+x = int( input("Enter rows: ") )
+y = int( input("Enter columns: ") )
+for rows in range(x):
+    for columns in range(y):
+        print("@",end='')
+    print()
