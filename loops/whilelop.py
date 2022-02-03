742d268dd15acdd66439dc65e5ae0cff72a8aa8d
ChucklesDroid <mj.akarsh@gmail.com>
2022-02-03 19:56:09 +0530

    Added while loops in python

---
+++ loops/whilelop.py
@@ -0,0 +1,15 @@
+#  while loops execute a block of code for as long it satisfies the condition
+
+#  An Example of infinite loop
+#  while 1:
+    #  print("Stuck in a while loop")
+
+#  An Example of finite loop 
+#  name = "" 
+#  while len(name) == 0 :
+    #  name = input("Enter name: ")
+
+#  An alternative way is as follows :-
+name = None
+while not name:
+    name = input("Enter name: ")
