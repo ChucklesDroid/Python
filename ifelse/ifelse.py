8ec10ec63aad3f330b3a372e52afa5c43a3967a9
ChucklesDroid <mj.akarsh@gmail.com>
2022-02-03 10:54:44 +0530

    Using if-else operators in python

---
+++ ifelse/ifelse.py
@@ -0,0 +1,13 @@
+#  if else statements in python 
+
+age = int(input("what is your age ? "))
+
+if age == 100:
+    print("You are ancient")
+elif age >= 18:
+    print("you are an adult")
+elif age > 12 and age < 18:
+    print("You are a teenager")
+else:
+    print("You are a cry baby")
+
