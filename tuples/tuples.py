83b7a3ede00ad590d502f1ce5d095c901c37ff5e
ChucklesDroid <mj.akarsh@gmail.com>
2022-02-06 11:53:24 +0530

    Added tuples in Python

---
+++ tuples/tuples.py
@@ -0,0 +1,11 @@
+#  Tuples :- Its a collection which is ordered, unchangeable and duplicates are allowed
+
+bio = ("Aakarsh", 21, "male")
+print(bio)
+
+#  Operations that can be performed on tuples is limited to 'index' and 'count' since 
+#  tuples cannot be manipulated
+
+print(bio.count(21)) #Displays the count of argument in tuple 'bio'
+print(bio.index("male")) #Displays the index of passed argument
+
