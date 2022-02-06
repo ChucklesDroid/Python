f5b0534e07a47f5c334faa856d8fb59768bb2fe9
ChucklesDroid <mj.akarsh@gmail.com>
2022-02-06 11:23:56 +0530

    Added 2D lists in Python

---
+++ lists/2d_lists.py
@@ -0,0 +1,18 @@
+#  2D lists : collection of lists
+
+
+breakfast = ["milk", "biscuit", "bananas"]
+lunch = ["chicken", "rice", "berries"]
+dinner = ["rotis", "snow eggs", "apricots"]
+
+menu = [breakfast, lunch, dinner]
+
+#  To print entire 2D list
+print(menu)
+
+#  We can use indexing to print elements of the list
+#  To print specific list in 2D lists
+print(menu[0])
+
+#  To print specific elements in the 2d list
+print("Element at index 0,2: "+menu[0][2])
