788dccc41b068f1e9b69c56262433f8769447107
ChucklesDroid <mj.akarsh@gmail.com>
2022-02-16 05:06:49 +0530

    added file moving capabilities in python

--- file_handling/mvfile.py
+++ file_handling/mvfile.py
@@ -1,21 +1,37 @@
-#  Importing os to move files
+# To move a file in python we make use of os module
 
-import os                   #for os.replace(src, dest) and os.path.exists
+import os                   #For moving a file
 
-src = 'mv.test'             #path of the source file 
-dest = r'..'                #path of the destionation file
-try:                        #try block
-    if os.path.exists(dest) and os.path.isfile(dest):
+src = 'mv.test'
+dest = r'../mv.test'
+#  Writing the code in try-except block
+#  First moving a file 
+try:
+    if os.path.exists(dest):
         print("file already exists")
-    elif not(os.path.exists(src)):
-        print("{} :does not exist".format(src))
     else:
         os.replace(src,dest)
-        print('file moved')
-except FileNotFoundError:   #file not found error
-        print("Error encounted: File not found-{}".format(src))
+        print(src + " moved")
+except FileNotFoundError:
+    print(src + " not found")
+except Exception:
+    print("Something went wrong")
+else:
+    print("No errors encountered")
+
+#  Moving a folder in python 
+src1 = 'testfldr'
+dest1 = r'../testfldr'
+try:
+    if os.path.exists(dest1):
+        print("Folder already exists")
+    else:
+        os.replace(src1,dest1)
+        print(src1 + " moved successfully")
+except FileNotFoundError:
+    print(src1 + " not found")
 except Exception as e:
-        print("Error detected: {}".format(e))
-        print("Something went wrong ):")
-finally:
-        print("Program executed")
+    print("Error encountered:{}".format(e))
+    print("Something went wrong (:")
+else:
+    print("No errors encountered")
