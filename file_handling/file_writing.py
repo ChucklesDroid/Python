989a2139bad7e8fff358fb1a7868eadd441e8a84
ChucklesDroid <mj.akarsh@gmail.com>
2022-05-06 06:47:37 +0530

    added writing/reading from a binary file

--- file_handling/file_writing.py
+++ file_handling/file_writing.py
@@ -35,3 +35,21 @@ if os.path.exists(path):
         print("Checking for proper handling of file {}".format(file.closed))
 else:
     print("Error encountered while opening a file ):")
+
+
+#  .tell gives an integer value when file is a binary file and returns an opaque number in case of text file
+#  .seek takes two arguments offset and whence. In case of binary file whence has an integer value 0,1 or 2
+#  indicating the beginning, current and ending position in the file. In case of text file whence only allows 
+#  value 0 and only in extreme case takes value 2( seek(0,2) for end of file ) and offset can only take value returned by .tell
+
+# Note:- While writing to a binary file we cannot write string it has to be
+#in byte like format.This can be achieved by writing 'b' in front of string
+#  Example:- b"this binary coded format"
+
+#  Writing/Reading from a binary file 
+with open('demo.txt', 'rb+') as file:
+    txt=b'This is {number} line'
+    for i in range(1,6):
+        file.write(txt.format(number=i).encode())
+    file.seek(0, 0)
+    print(file.read().decode())
