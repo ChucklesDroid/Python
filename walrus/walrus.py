44aadb7ca5e10aa49e5b840f4cdaf1ed1f5e6dd7
ChucklesDroid <mj.akarsh@gmail.com>
2022-05-08 20:39:15 +0530

    walrus operator

---
+++ walrus/walrus.py
@@ -0,0 +1,15 @@
+#  walrus operator:-
+#  new to python 3.8
+#  assignment operator aka walrus operator assigns values to variables as part of a larger expression
+
+#  foods = list()
+#  while True:
+    #  food = input("What food do you like ?:")
+    #  if food.lower() == "quit":
+        #  break
+    #  foods.append(food)
+
+foods = list()
+while food:=input("What food do you like ?:") != "quit":
+    foods.append(food)
+
