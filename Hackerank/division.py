9fa1a14b2414b3a61c96bd1fe909f02f8338b000
ChucklesDroid <mj.akarsh@gmail.com>
2022-05-15 16:32:53 +0530

    added division in python

---
+++ Hackerank/division.py
@@ -0,0 +1,8 @@
+#  From python 3 onwards python provides specific operators for both integer and floating point division. 
+#  operator '//' is used for integer division and operator '/' is used for floating point division
+
+a = int(input())
+b = int(input())
+
+print(a//b)
+print(a/b)
